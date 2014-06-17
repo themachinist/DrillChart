0218a1be411f7920ec99c705a5656dacb9bcb908
David Winslow <the@machini.st>
2014-06-17 09:01:29 -0700

    figuratin issure

--- ext.DrillChart.js
+++ ext.DrillChart.js
@@ -1,31 +1,30 @@
-$( document ).ready(function() {
-	mw.loader.using( ['flexigrid'], function () {
-		$('.flexigrid').flexigrid({
-			url: 'extensions/DrillChart/DrillChart.php?data=1',
-			dataType: 'json',
-			colModel : [
-			{display: 'Imperial', name : 'imperial', width : 80, sortable : true, align: 'right'},
-			{display: 'Wire', name : 'wire', width : 80, sortable : true, align: 'right'},
-			{display: 'Metric', name : 'metric', width : 80, sortable : true, align: 'right'},
-			{display: 'Tap Sizes', name : 'tap', width : 130, sortable : true, align: 'left'},
-			{display: 'STI Sizes', name : 'sti', width : 80, sortable : true, align: 'left'}
-			],
-			searchitems : [
-			{display: 'Imperial', name : 'imperial', isdefault: true},
-			{display: 'Wire', name : 'wire'},
-			{display: 'Metric', name : 'metric'},
-			{display: 'Tap Sizes', name : 'tap'},
-			{display: 'STI Sizes', name : 'sti'}
-			],
-			sortname: "imperial",
-			sortorder: "asc",
-			usepager: false,
-			title: 'Drill and Tap-Drill Sizes',
-			useRp: false,
-			rp: 15,
-			showTableToggleBtn: true,
-			width: 700,
-			height: 600
-		});
+mw.loader.using( ['flexigrid'], function () {
+	$('.flexigrid').flexigrid({
+		url: 'extensions/DrillChart/DrillChart.php?data=1',
+		dataType: 'json',
+		colModel : [
+		{display: 'Imperial', name : 'imperial', width : 80, sortable : true, align: 'right'},
+		{display: 'Wire', name : 'wire', width : 80, sortable : true, align: 'right'},
+		{display: 'Metric', name : 'metric', width : 80, sortable : true, align: 'right'},
+		{display: 'Tap Sizes', name : 'tap', width : 130, sortable : true, align: 'left'},
+		{display: 'STI Sizes', name : 'sti', width : 80, sortable : true, align: 'left'}
+		],
+		searchitems : [
+		{display: 'Imperial', name : 'imperial', isdefault: true},
+		{display: 'Wire', name : 'wire'},
+		{display: 'Metric', name : 'metric'},
+		{display: 'Tap Sizes', name : 'tap'},
+		{display: 'STI Sizes', name : 'sti'}
+		],
+		sortname: "imperial",
+		sortorder: "asc",
+		usepager: false,
+		title: 'Drill and Tap-Drill Sizes',
+		useRp: false,
+		rp: 15,
+		showTableToggleBtn: true,
+		width: 700,
+		height: 600
 	});
 });
+
