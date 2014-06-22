f7513f8e79ed4b257d12388cf8af4d13b41abd9b
yoitsdaedalus <yo@itsdaedal.us>
2014-06-22 13:38:19 -0700

    uplarding some logic to make sure flexigrid has loaded. mw.loader.using is apparently unreliable, but that probably has to do with the order that flexigrid initilizes. should probably look at flexigrid code

--- ext.DrillChart.js
+++ ext.DrillChart.js
@@ -1,45 +1,47 @@
 mw.loader.using( 'jquery', function(){
 	console.log('jquery ready');
 	mw.loader.using( 'flexigrid', function(){
-		console.log('flexigrid ready');
-		console.log($.flexigrid);
-		$('.flexigrid').flexigrid({
-			url: 'extensions/DrillChart/ChartQuery.php',
-			dataType: 'json',
-			procmsg: 'Processing, Please wait ...',
-			onSuccess:function(){
-                console.log('successfully loaded data');
-            },
-            onError: function(data){
-			    console.log('something is wrong');
-			    console.log(data);
-			},
-			colModel : [
-			{display: 'Imperial', name : 'imperial', width : 80, sortable : true, align: 'right'},
-			{display: 'Wire', name : 'wire', width : 80, sortable : true, align: 'right'},
-			{display: 'Metric', name : 'metric', width : 80, sortable : true, align: 'right'},
-			{display: 'Tap Sizes', name : 'tap', width : 130, sortable : true, align: 'left'},
-			{display: 'STI Sizes', name : 'sti', width : 80, sortable : true, align: 'left'}
-			],
-			searchitems : [
-			{display: 'Imperial', name : 'imperial', isdefault: true},
-			{display: 'Wire', name : 'wire'},
-			{display: 'Metric', name : 'metric'},
-			{display: 'Tap Sizes', name : 'tap'},
-			{display: 'STI Sizes', name : 'sti'}
-			],
-			sortname: "imperial",
-			sortorder: "asc",
-			usepager: true,
-			title: 'Drill and Tap-Drill Sizes',
-			useRp: false,
-			rp: 15,
-			showTableToggleBtn: true,
-			width: 700,
-			height: 400
-		});
-		console.log('after flexigrid call');
-		console.log($.flexigrid);
+		var testFunctionExists = setInterval(function(){
+			console.log('testing for .flexigrid()');
+			if (typeof $.flexigrid != 'undefined'){
+				console.log('flexigrid exists!');
+				$('.flexigrid').flexigrid({
+					url: 'extensions/DrillChart/ChartQuery.php',
+					dataType: 'json',
+					procmsg: 'Processing, Please wait ...',
+					onSuccess:function(){
+						console.log('successfully loaded data');
+					},
+					onError: function(data){
+						console.log('something is wrong');
+						console.log(data);
+					},
+					colModel : [
+					{display: 'Imperial', name : 'imperial', width : 80, sortable : true, align: 'right'},
+					{display: 'Wire', name : 'wire', width : 80, sortable : true, align: 'right'},
+					{display: 'Metric', name : 'metric', width : 80, sortable : true, align: 'right'},
+					{display: 'Tap Sizes', name : 'tap', width : 130, sortable : true, align: 'left'},
+					{display: 'STI Sizes', name : 'sti', width : 80, sortable : true, align: 'left'}
+					],
+					searchitems : [
+					{display: 'Imperial', name : 'imperial', isdefault: true},
+					{display: 'Wire', name : 'wire'},
+					{display: 'Metric', name : 'metric'},
+					{display: 'Tap Sizes', name : 'tap'},
+					{display: 'STI Sizes', name : 'sti'}
+					],
+					sortname: "imperial",
+					sortorder: "asc",
+					usepager: true,
+					title: 'Drill and Tap-Drill Sizes',
+					useRp: false,
+					rp: 15,
+					showTableToggleBtn: true,
+					width: 700,
+					height: 400
+				});
+			}
+		}, 100);
 	});
 });
 
