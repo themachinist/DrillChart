77f3b606028034e4d035bf920562b29d1feb01ba
David Winslow <the@machini.st>
2014-06-24 10:34:09 -0700

    added a procedure to the sortBy method to renumber the id of each row according to the array order after uasort()

--- ChartQuery.class.php
+++ ChartQuery.class.php
@@ -57,7 +57,13 @@ class ChartQuery {
 				return $comp * -1;
 			}
 		});
-
+		// it doesn't feel like great form to do this
+		// maybe i can figure out how to work this into sort function
+		$i = 1;
+		foreach ($this->view['rows'] as $row){
+			$row['id'] = $i;
+			$i++;
+		}
 		return $this;
 	}
 
