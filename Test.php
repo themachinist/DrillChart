e8f8abdd47082dc211dfcf59ecc2ba9771e6b6af
David Winslow <the@machini.st>
2014-06-24 10:57:55 -0700

    change test file

--- Test.php
+++ Test.php
@@ -2,8 +2,8 @@
 header("access-control-allow-origin: *");
 $_POST['page'] = 1;
 $_POST['rp'] = 5;
-$_POST['sortname'] = 'metric';
-$_POST['sortorder'] = 'desc';
+$_POST['sortname'] = 'tap';
+$_POST['sortorder'] = 'asc';
 $_POST['query'] = '0.01';
 $_POST['qtype'] = 'imperial';
 require('ChartQuery.php');
